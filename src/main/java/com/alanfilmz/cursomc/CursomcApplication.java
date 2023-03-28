d8584ddde26d51f45873426a49f48a3d7531347e
AlanFilmz <alanfilmz@gmail.com>
2023-03-27 22:49:11 -0300

    Ajustes no endpoint/categorias/{id}

--- src/main/java/com/alanfilmz/cursomc/CursomcApplication.java
+++ src/main/java/com/alanfilmz/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.alanfilmz.cursomc;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
--- src/main/java/com/alanfilmz/cursomc/services/CategoriaService.java
+++ src/main/java/com/alanfilmz/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.alanfilmz.cursomc.services;
 
 import java.util.Optional;
 
+import com.alanfilmz.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+		
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
+	
 }
+
